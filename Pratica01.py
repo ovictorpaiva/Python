cd7467c45628cf5f336c5591fc088801f1656069
Victor Paiva <victor.eagpaiva@gmail.com>
2025-03-07 22:47:00 -0300

    Desafio01 Python EdN

--- Pratica01.py
+++ Pratica01.py
@@ -1,42 +1,26 @@
-##Prática 01:
-
-'''1- Programa de Saudação
-* Crie um programa que imprima a mensagem "Olá, mundo!" na tela.'''
-
+##Prática 01: Programação em Python
+##1- Programa de Saudação  
 print("Olá, mundo!")
 
-'''2- Calculadora de Soma
-* Desenvolva um programa que soma dois números. Use as variáveis numero1 = 12 e numero2 = 14. O programa deve calcular a soma e exibir o resultado.'''
-
+##2- Calculadora de Soma
 num1 = 12
 num2 = 14
 soma = num1 + num2
 print(soma)
 
-'''3- Calculadora de Volume
-* Crie um programa que calcula o volume de uma caixa retangular. Use as seguintes dimensões:
-
-* Comprimento: 12 cm
-* Largura: 14 cm
-* Altura: 20 cm
-O programa deve calcular o volume e exibir o resultado em cm³.'''
-
+#3- Calculadora de Volume
 comprimento = 12
 largura = 14
 altura = 20
 volume = comprimento * largura * altura
-print(volume +'cm³')
+print(f'O volume do retangulo é: {volume} cm³')
 
-'''4- Calculadora de Preço Total
-* Desenvolva um programa que calcula o preço total de uma compra. Use as seguintes informações:
-
-* Nome do produto: "Cadeira Infantil"
-* Preço unitário: R$ 12.40
-* Quantidade: 3
-O programa deve calcular o preço total e exibir todas as informações, incluindo o resultado final.'''
-
+#4- Calculadora de Preço Total
 nome_produto = "Cadeira Infantil"
 preco_unitario = 12.40
 quantidade = 3
 preco_total = preco_unitario * quantidade
-print(f"Produto: {nome_produto}\nPreço unitário: R$ {preco_unitario}\nQuantidade: {quantidade}\nPreço total: R$ {preco_total}")
+print(f'Produto: {nome_produto}')
+print(f'Preço unitário: R$ {preco_unitario:.2f}')
+print(f'Quantidade: {quantidade}')
+print(f'Preço total: R$ {preco_total:.2f}')
